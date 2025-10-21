f68e8f37817d67499fd72372e73c261ef48f386c
GiovanniPereira05 <giovanni.pereira.15977@gmail.com>
2025-10-21 11:06:48 -0300

    excluir dataset simulação

---
+++ js/dataset.js
@@ -0,0 +1,5 @@
+let lixeira_button = document.getElementById('lixeira-button')
+lixeira_button.addEventListener('click', () => {
+    alert('Dataset Excluido')
+    window.location.href = '../index.html'
+})
